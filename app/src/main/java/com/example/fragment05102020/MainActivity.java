8fb41bcbea4cecde139de6cc111ed52595281455
phatdroid94 <phatdroid94@gmail.com>
2021-01-15 20:37:25 +0700

    finish

--- app/src/main/java/com/example/fragment05102020/MainActivity.java
+++ app/src/main/java/com/example/fragment05102020/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.fragment05102020;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    FragmentManager mFragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+        AndroidFragment androidFragment = new AndroidFragment();
+        fragmentTransaction.add(R.id.linearContainer,androidFragment);
+        fragmentTransaction.commitNow();
+
+        Log.d("BBB",mFragmentManager.getFragments().size() + "");
     }
 }
